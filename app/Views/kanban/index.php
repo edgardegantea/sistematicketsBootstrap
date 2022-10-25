22d315f56ecf73c83a0c040e00eb99fb52bbe087
edgardegantea <edgar.degante.a@gmail.com>
2022-10-24 22:15:39 -0500

    Actualización de vista de kanban

--- app/Views/kanban/index.php
+++ app/Views/kanban/index.php
@@ -15,22 +15,22 @@
 <section class="section">
     <div class="row">
         <nav class="level">
-            <div class="level-item has-text-centered">
-                <div>
+            <div class="level-item has-text-centered  mgh-medium">
+                <div class="card-content has-background-primary has-text-light">
                     <p class="heading">TICKETS NO INICIADOS</p>
-                    <p class="title has-text-danger"><?php echo $ticketsNoIniciados; ?></p>
+                    <p class="title has-text-white"><?php echo $ticketsNoIniciados; ?></p>
                 </div>
             </div>
-            <div class="level-item has-text-centered">
-                <div>
+            <div class="level-item has-text-centered mgh-medium">
+                <div class="card-content has-background-link has-text-light">
                     <p class="heading">TICKETS EN SEGUIMIENTO</p>
-                    <p class="title"><?php echo $ticketsEnSeguimiento; ?></p>
+                    <p class="title has-text-white"><?php echo $ticketsEnSeguimiento; ?></p>
                 </div>
             </div>
-            <div class="level-item has-text-centered">
-                <div>
+            <div class="level-item has-text-centered mgh-medium">
+                <div class="card-content has-background-black has-text-light">
                     <p class="heading">TICKETS CERRADOS</p>
-                    <p class="title has-text-primary"><?php echo $ticketsCerrados; ?></p>
+                    <p class="title has-text-white"><?php echo $ticketsCerrados; ?></p>
                 </div>
             </div>
             
@@ -89,7 +89,7 @@
                         <?php if (!empty($ts02) && is_array($ts02)): ?>
                             <?php foreach ($ts02 as $t2): ?>
                                 <div class="box">
-                                    <a href="<?= base_url('tickets/show/' . $t2['id']); ?>" style="text-decoration: none">
+                                    <a href="<?= base_url('tickets/' . $t2['id']); ?>" style="text-decoration: none">
                                         <?= esc($t2['title']) ?>
                                     </a>
                                 </div>
@@ -108,9 +108,15 @@
                         <?php if (!empty($ts03) && is_array($ts03)): ?>
                             <?php foreach ($ts03 as $t3): ?>
                                 <div class="box">
-                                    <a href="<?= base_url('tickets/show/' . $t3['id']); ?>" style="text-decoration: none">
+                                    <a href="<?= base_url('tickets/' . $t3['id']); ?>" style="text-decoration: none">
                                         <?= esc($t3['title']) ?>
                                     </a>
+
+                                    <!-- 
+                                    <a class="is-small has-text-black" href="<?= base_url('tickets/'.$t3['id']); ?>">
+                                        <?= esc($t3['title']) ?>
+                                    </a>
+                                -->
                                 </div>
                             <?php endforeach ?>
                         <?php endif; ?>
